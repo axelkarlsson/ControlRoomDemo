e69f76ceb7efcd0f8fb792ba3d748fbfb70dacdb
Axel <axeka054@student.liu.se>
2017-07-04 16:19:10 +0200

    Can't open demo objext
    Scene can be loaded and unloaded, the demo object raises an error because OnTap has no receiver (In editor)

--- Assets/SceneManager_.cs
+++ Assets/SceneManager_.cs
@@ -8,18 +8,22 @@ public class SceneManager_ : MonoBehaviour {
     public static bool hasLoaded = false;
 	// Use this for initialization
 	void Start () {
-	}
+        hasLoaded = PlaceDemoObject(new Vector3(0, 0, 2));
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
+        Debug.Log("We are getting somewhere");
 		if (SceneManager.GetActiveScene().name == "Test" &&  !hasLoaded)
         {
             hasLoaded = PlaceDemoObject(new Vector3(0,0,2));
         }
+        */
 	}
     bool PlaceDemoObject(Vector3 pos)
     {
-        GameObject g = Instantiate(Resources.Load("ObjectName") as GameObject, null);
+        GameObject g = Instantiate(Resources.Load("ObjectName") as GameObject, transform);
         g.name = "Demo Object";
         g.transform.position = pos;
         g.SetActive(true);
