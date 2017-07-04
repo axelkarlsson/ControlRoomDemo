49e9ebc58ac2dd2c69092dc4d16e560dbf2db97a
Iacentis <CarlJFandersson@gmail.com>
2017-07-04 11:01:25 +0200

    DatBug
    Is Fixed

---
+++ Assets/TempPlaneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class TempPlaneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<TapToPlace>().IsBeingPlaced = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
