dfd044cf0885e51fb397c82d7dcb5a2af32b384d
Axel <axeka054@student.liu.se>
2017-07-07 13:50:22 +0200

    Renamed dir to remove spaces
    Is this better now git????

---
+++ Assets/AspectObjectSceneAssets/Scripts/Controllers/AspectWindowController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class AspectWindowController : MonoBehaviour {
+
+    TapToPlace Placer;
+    // Use this for initialization
+    void Start () {
+        Placer = GetComponentInChildren<TapToPlace>();
+        Placer.OnInputClicked(eventData: null);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
